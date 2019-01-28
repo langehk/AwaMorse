63d65af0e6cc3191cda9a824cda10534f3fd103f
langehk <rupkri32@gmail.com>
2019-01-28 10:37:06 +0100

    init commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBtiuVXYzMQxtJNcJoOeJ2wNNVLrs3o8xk',
+    authDomain: 'awamorsecode.firebaseapp.com',
+    databaseURL: 'https://awamorsecode.firebaseio.com',
+    projectId: 'awamorsecode',
+    storageBucket: '',
+    messagingSenderId: '1056158566367'
+  }
+};
