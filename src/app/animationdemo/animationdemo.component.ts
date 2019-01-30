f208a0ac70ed0508f5b499b0db0b6ff7ad025660
langehk <rupkri32@gmail.com>
2019-01-30 10:08:44 +0100

    navbar + animation opstart

---
+++ src/app/animationdemo/animationdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animationdemo',
+  templateUrl: './animationdemo.component.html',
+  styleUrls: ['./animationdemo.component.scss']
+})
+export class AnimationdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
@@ -10,21 +14,29 @@ import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import { NewMessageComponent } from './new-message/new-message.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './message/shared/navbar/navbar.component';
+import { Component, HostBinding } from '@angular/core';
+import {animate, transition, trigger} from '@angular/animations';
+import { AnimationdemoComponent } from './animationdemo/animationdemo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewMessageComponent
+    NewMessageComponent,
+    NavbarComponent,
+    AnimationdemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    MomentModule, AppRoutingModule
+    MomentModule, AppRoutingModule, TooltipModule.forRoot(), ButtonsModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [
-    MessageService
+    MessageService,
   ],
   bootstrap: [AppComponent]
 })
