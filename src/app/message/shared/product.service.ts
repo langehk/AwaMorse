510bec847920adfff0d7eaebfc79835a401cf9dc
langehk <rupkri32@gmail.com>
2019-02-06 11:01:36 +0100

    product CRUD from fireStore

--- src/app/message/shared/product.service.ts
+++ src/app/message/shared/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs/internal/Observable';
+import {Product} from './product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,8 +22,41 @@ export class ProductService {
     return this.db.collection('products').valueChanges();
   }
 
+  getProductById(id: string) {
+    this.db.doc<Product>('products/' + id).get()
+      .subscribe(productFound => {
 
+        const protData = productFound.data() as Product;
+      });
+  }
+
+  update(product: Product) {
+    this.db.doc<Product>('products/' + product.id)
+      .update(product)
+      .then(prod => {
+        debugger;
+      });
+  }
+
+  delete(product: Product) {
+    this.db.doc<Product>('products/' + product.id)
+      .delete()
+      .then(prod => {
+        debugger;
+      });
+  }
+
+  add(product: Product) {
+    this.db.collection<Product>('products')
+      .add(product)
+      .then(prod => {
+        debugger;
+      });
+  }
 }
+
+
+
 
 
 
--- src/app/wild/wild.component.ts
+++ src/app/wild/wild.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../message/shared/product.service';
 import {ObservableInput} from 'rxjs/internal/types';
+import {Product} from '../message/shared/product.model';
 
 @Component({
   selector: 'app-wild',
@@ -20,8 +21,21 @@ export class WildComponent implements OnInit {
     this.products = this.ps.getProducts();
   }
 
-  deleteProducts() {
-    this.products = this.ps.deleteProducts();
+  deleteProducts(product: Product) {
+    this.ps.delete(product);
   }
+
+  updateProduct(product: Product)
+  {
+    product.name = product.name + '1';
+    this.ps.update(product);
+  }
+
+  addProduct(){
+    this.ps.add({name: 'New Product'});
+  }
+
+
+
 
 }
